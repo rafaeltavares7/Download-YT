606b273db15fb519d7845281914bb17c44a504c0
Rafael <168600337+rafaeltavares7@users.noreply.github.com>
2025-04-27 13:10:07 -0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -18,7 +18,7 @@ elif [ "$1" == "---mp3" ]; then
   yt-dlp -o "%(title)s.%(ext)s" -x --audio-format mp3 --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" --no-check-certificate --verbose "$video"; deactivate
 
 elif [ "$1" == "-h" ]; then
-  echo "download_yt.sh ---video [URL]"
-  echo "download_yt.sh ---m4a [URL]"
-  echo "download_yt.sh ---mp3 [URL]"
+  echo "download.sh ---video [URL]"
+  echo "download.sh ---m4a [URL]"
+  echo "download.sh ---mp3 [URL]"
 fi
